46158de2753d20310dbbc209c9abfc56f83e1770
piolin666satan <santiag.2016@gmail.com>
2026-03-30 01:59:11 -0500

    añadiendo mapeo a las entidades baseEntity y Categoria, añadiendo tambien la base de datos .env y un enum de tipo de identidad.

--- src/main/java/com/grupo6/biblioteca_digital/model/entity/BaseEntity.java
+++ src/main/java/com/grupo6/biblioteca_digital/model/entity/BaseEntity.java
@@ -1,8 +1,13 @@
 package com.grupo6.biblioteca_digital.model.entity;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.*;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Setter;
 import lombok.Getter;
@@ -12,4 +17,16 @@ import lombok.Getter;
 @Getter
 @Setter
 public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "fecha_registro", nullable = false, updatable = false)
+    private LocalDateTime fechaRegistro;
+
+    @LastModifiedDate
+    @Column(name = "fecha_modificacion")
+    private LocalDateTime fechaModificacion;
 }
